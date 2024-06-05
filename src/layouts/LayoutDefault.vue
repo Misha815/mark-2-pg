3c0102bf38d61c4c55d250521fa90668be800cd0
Misha815 <msha.stepnov.01@mail.ru>
2024-06-06 00:07:22 +0300

    06.06.2024

--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -7,9 +7,10 @@
           <router-link to="/Men">Men</router-link>
           <router-link to="/Women">Women</router-link>
         </div>
+        <div class="catalog-mb"><img src="../assets/images/catalog-mb.svg" alt="catalog"></div>
         <div class="logo"><router-link to="/"><img src="../assets/images/Logo.svg" alt="KICKS"></router-link></div>
         <div class="user-bar">
-          <button><img src="../assets/images/Search.svg" alt="serch"></button>
+          <button class="seacrh"><img src="../assets/images/Search.svg" alt="serch"></button>
           <button><img src="../assets/images/UserAccount.svg" alt="user"></button>
           <span class="calc-trash">0</span>
         </div>
@@ -74,14 +75,12 @@
                 <h5>Follow us</h5>
                 <ul>
                   <ul class="icon-ul">
-                    <li> <a href="#"> <img src="../assets/images/facebook.svg" alt="KICKS"></a></li>
+                    <li><a href="#"> <img src="../assets/images/facebook.svg" alt="KICKS"></a></li>
                     <li><a href="#"> <img src="../assets/images/instagram.svg" alt="KICKS"></a></li>
-                    <li> <a href="#"> <img src="../assets/images/twitter.svg" alt="KICKS"></a></li>
-                    <li> <a href="#"> <img src="../assets/images/tiktok.svg" alt="KICKS"></a></li>
+                    <li><a href="#"> <img src="../assets/images/twitter.svg" alt="KICKS"></a></li>
+                    <li><a href="#"> <img src="../assets/images/tiktok.svg" alt="KICKS"></a></li>
                   </ul>
-
                 </ul>
-
               </div>
             </div>
           </div>
@@ -119,6 +118,10 @@ export default {
       width: 301px;
       display: flex;
       justify-content: space-evenly;
+    }
+
+    .catalog-mb {
+      display: none;
     }
 
     .logo {
@@ -168,20 +171,22 @@ footer {
         width: 510px;
 
         .Join {
-          font-size: 48px;
+          font-size: clamp(2rem, 1.4681rem + 1.7021vw, 3rem);
           color: #ffffff;
           font-weight: 500;
         }
 
         p {
-          font-size: 20px;
+          font-size: clamp(1rem, 0.867rem + 0.4255vw, 1.25rem);
           color: #ffffff;
         }
 
         .join-form {
           input {
 
-            width: 342px;
+            max-width: 342px;
+            width: 100%;
+
             height: 48px;
             border: 2px solid #ffffff;
             color: #fff;
@@ -210,7 +215,6 @@ footer {
 
             }
           }
-
         }
 
         .sale_lable {
@@ -226,32 +230,27 @@ footer {
     background-color: #232321;
     border-radius: 48px;
     position: relative;
-
     color: #fff;
 
     .container {
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-      grid-template-rows: 1fr;
-      gap: 30px 30px;
-
-
       display: flex;
-      justify-content: space-between;
-      padding: 40px;
+      justify-content: space-around;
 
+
+      padding: 40px;
 
       .about {
         .about__inner {
           max-width: 446px;
 
           h5 {
-            font-size: 36px;
+            font-size: clamp(1.5625rem, 1.1968rem + 1.1702vw, 2.25rem);
+            font-weight: 600;
             color: #FFA52F;
           }
 
           p {
-            font-size: 20px;
+            font-size: clamp(1rem, 0.867rem + 0.4255vw, 1.25rem);
 
           }
         }
@@ -259,10 +258,9 @@ footer {
 
       .categories {
         .categories__inner {
-
-
           h5 {
-            font-size: 24px;
+            font-weight: 600;
+            font-size: clamp(1.25rem, 1.117rem + 0.4255vw, 1.5rem);
             color: #FFA52F;
           }
 
@@ -274,20 +272,25 @@ footer {
             a {
               font-weight: 400;
               color: #fff;
+              font-size: clamp(1rem, 0.867rem + 0.4255vw, 1.25rem);
+
+              &:hover {
+                color: #FFA52F;
+              }
 
               img {}
             }
 
             .icon-ul {
+
+
               display: flex;
               justify-content: space-between;
 
-              li {}
+              li {
+                margin: 3px;
+              }
             }
-
-
-
-
           }
         }
       }
@@ -315,21 +318,247 @@ footer {
   justify-content: center;
 
   span {
-    font-size: 24px;
+    font-size: clamp(0.875rem, 0.5426rem + 1.0638vw, 1.5rem);
   }
 }
 
-@media (max-width:800px) {
-  .footer__sale {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
 
-    max-width: 1320px;
-    height: 340px;
-    background-color: #4A69E2;
-    border-radius: 48px 48px 0 0;
+
+@media (max-width: 1200px) {}
+
+@media (max-width: 800px) {}
+
+@media (max-width: 500px) {
+  .header {
+    position: relative;
+    top: 0px;
+
+    .nav {
+      background-color: #ffffff;
+      border-radius: 24px;
+      max-width: 1320px;
+      height: 96px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+
+      .catalog {
+        width: 301px;
+        display: none;
+        justify-content: space-evenly;
+      }
+
+      .catalog-mb {
+        display: flex;
+
+      }
+
+      .logo {
+        width: 128px;
+      }
+
+      .user-bar {
+        width: auto;
+        display: flex;
+        justify-content: space-evenly;
+
+        .seacrh {
+          display: none;
+        }
+
+        .calc-trash {
+          background-color: #FFA52F;
+          height: 32px;
+          width: 32px;
+          border-radius: 32px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-weight: 500;
+
+        }
+      }
+    }
   }
 
+  footer {
+    margin-top: 128px;
+
+    .footer__contaier {
+      max-width: 1320px;
+
+      background-color: #4A69E2;
+      border-radius: 48px;
+
+      .footer__sale {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        max-width: 1320px;
+        height: 340px;
+        background-color: #4A69E2;
+        border-radius: 48px 48px 0 0;
+
+        .sale__form {
+
+          width: 326px;
+          display: flex;
+          flex-direction: column;
+
+
+
+          .Join {
+            font-size: clamp(2rem, 1.4681rem + 1.7021vw, 3rem);
+            color: #ffffff;
+            font-weight: 500;
+
+          }
+
+          p {
+            font-size: clamp(1rem, 0.867rem + 0.4255vw, 1.25rem);
+            color: #ffffff;
+          }
+
+          .join-form {
+            input {
+
+              max-width: 342px;
+              width: 100%;
+
+              height: 48px;
+              border: 2px solid #ffffff;
+              color: #fff;
+              border-radius: 8px;
+              font-size: 20px;
+              background-color: #4A69E2;
+              outline: none;
+            }
+
+            .submit-btn {
+              color: #ffffff;
+              background-color: #232321;
+              width: 102px;
+              height: 48px;
+              border-radius: 8px;
+              text-align: center;
+
+              font-weight: 500;
+
+              margin-left: 4px;
+
+              &:hover {
+                background-color: #ffffff;
+                color: #232321;
+                outline: 2px solid #232321;
+
+              }
+            }
+          }
+
+          .sale_lable {
+            img {}
+          }
+        }
+      }
+    }
+
+    .footer__inner {
+      max-width: 1320px;
+      height: 591px;
+      background-color: #232321;
+      border-radius: 48px;
+      position: relative;
+      color: #fff;
+
+      .container {
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+
+        padding: 40px;
+
+        .about {
+          .about__inner {
+            max-width: 446px;
+
+            h5 {
+              font-size: clamp(1.5625rem, 1.1968rem + 1.1702vw, 2.25rem);
+              font-weight: 600;
+              color: #FFA52F;
+            }
+
+            p {
+              font-size: clamp(1rem, 0.867rem + 0.4255vw, 1.25rem);
+
+            }
+          }
+        }
+
+        .categories {
+          .categories__inner {
+            h5 {
+              font-weight: 600;
+              font-size: clamp(1.25rem, 1.117rem + 0.4255vw, 1.5rem);
+              color: #FFA52F;
+            }
+
+            ul {
+              all: unset;
+
+              list-style-type: none;
+
+              a {
+                font-weight: 400;
+                color: #fff;
+                font-size: clamp(1rem, 0.867rem + 0.4255vw, 1.25rem);
+
+                &:hover {
+                  color: #FFA52F;
+                }
+
+                img {}
+              }
+
+              .icon-ul {
+
+
+                display: flex;
+                justify-content: start;
+
+                li {
+                  margin: 3px;
+                }
+              }
+            }
+          }
+        }
+
+
+      }
+
+      .footer-logo {
+        display: flex;
+        justify-content: center;
+        z-index: 66;
+
+        img {
+          position: absolute;
+          bottom: 0;
+          max-width: 80%;
+          object-fit: cover;
+        }
+      }
+    }
+  }
+
+  .rights-bar {
+    display: flex;
+    justify-content: center;
+
+    span {
+      font-size: clamp(0.875rem, 0.5426rem + 1.0638vw, 1.5rem);
+    }
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,80 +1,17 @@
 <template>
   <layout-default>
-    <div class="conteiner-header">
-      <h1>DO IT <span>RIGHT</span></h1>
-      <div class="container-header__content">
-        <div class="shop-now">
-          <span class="title-span">NIKE AIR MAX</span>
-          <span>Nike introducing the new air max for everyone's comfort</span>
-          <button>SHOP NOW</button>
-        </div>
-      </div>
-    </div>
+    <home-titles />
   </layout-default>
 
 </template>
 
 <script>
+import HomeTitles from '@/components/HomeTitles.vue'
 import LayoutDefault from '../layouts/LayoutDefault.vue'
 export default {
-  components: { LayoutDefault },
+  components: { LayoutDefault, HomeTitles },
 
 }
 </script>
 
-<style lang="scss" scoped>
-.conteiner-header {
-  h1 {
-    font-size: clamp(1rem, -5.8983rem + 22.0745vw, 13.96875rem);
-    font-weight: bold;
-
-    span {
-      color: #4A69E2;
-      font-weight: bold;
-    }
-  }
-
-  .container-header__content {
-    max-width: 1320px;
-    height: 750px;
-    border-radius: 64px;
-    background-color: #70706E;
-    background-image: url('../assets/images/Nike Air Max poster.png');
-    display: flex;
-    flex-direction: column;
-
-    justify-content: end;
-
-    .shop-now {
-      margin: 48px;
-      display: flex;
-      flex-direction: column;
-      max-width: 490px;
-      justify-content: end;
-
-      .title-span {
-        font-size: 74px;
-      }
-
-      span {
-        color: #FFF;
-        font-size: 24px;
-      }
-
-      button {
-        width: 138px;
-        height: 48px;
-        border-radius: 8px;
-        border: none;
-        background-color: #4A69E2;
-        color: #FFF;
-        text-align: center;
-
-        &:hover {
-          background-color: #232321;
-        }
-      }
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
